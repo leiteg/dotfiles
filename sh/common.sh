90b0cf86b52b1bda118dcf6922b7e4aa21674ada
Gustavo Leite <gustavoleite.ti@gmail.com>
2019-11-26 12:56:59 -0300

    Add shell function `tsh`: connect ssh and attach tmux

--- sh/common.sh
+++ sh/common.sh
@@ -14,3 +14,10 @@ alias quit="exit"
 
 # User environment variables
 export PATH="$PATH:$HOME/bin"
+
+# Connect to a server using SSH an automatically attach/create session
+function tsh {
+    host=$1
+    session=${2:-main}
+    ssh $host -t "tmux attach -t $session || tmux new -s $session";
+}
