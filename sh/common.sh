68d860c8f7906cdc4421012fdeabebe2b4d71410
Gustavo Leite <gustavoleite.ti@gmail.com>
2023-07-06 10:16:28 -0300

    sh: change tmux alias to `t`

--- sh/common.sh
+++ sh/common.sh
@@ -154,7 +154,7 @@ try_alias ls    exa
 try_alias cat   bat
 try_alias vim   nvim
 try_alias grep  rg
-try_alias tm    tmux
+try_alias t     tmux
 
 # VARIABLES --------------------------------------------------------------- {{{1
 
