09e727b968e0d4527670f3e77dea578904143a0a
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-11 13:38:02 -0300

    nvim: use lua table notation in settings

--- nvim/lua/leiteg/settings.lua
+++ nvim/lua/leiteg/settings.lua
@@ -21,7 +21,7 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
-vim.opt.colorcolumn = "81"
+vim.opt.colorcolumn = { 81 }
 vim.opt.wrap = false
 
 -- Search
