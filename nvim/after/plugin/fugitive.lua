e28ed58ea4a9e6c5bd40f6580e232a20d27dab7d
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-10 21:01:52 -0300

    nvim: run fugitive commands in new tab

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,16 +1,16 @@
 vim.keymap.set("n", "<leader><leader>", vim.cmd.Git, {
     desc = "Fugitive Status"
 })
-vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>", {
+vim.keymap.set("n", "<leader>gc", "<cmd>tab Git commit<CR>", {
     desc = "Fugitive Commit"
 })
-vim.keymap.set("n", "<leader>gr", "<cmd>Git review<CR>", {
+vim.keymap.set("n", "<leader>gr", "<cmd>tab Git diff --staged<CR>", {
     desc = "Fugitive Review"
 })
-vim.keymap.set("n", "<leader>gd", "<cmd>Git diff<CR>", {
+vim.keymap.set("n", "<leader>gd", "<cmd>tab Git diff<CR>", {
     desc = "Fugitive Diff"
 })
-vim.keymap.set("n", "<leader>gl", "<cmd>Git log --oneline<CR>", {
+vim.keymap.set("n", "<leader>gl", "<cmd>tab Git log --oneline<CR>", {
     desc = "Fugitive Log"
 })
 vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<CR>", {
