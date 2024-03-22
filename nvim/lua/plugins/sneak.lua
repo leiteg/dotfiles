93ff1542a45508155ad10767c472c0f112bc15ec
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-22 20:24:55 -0300

    nvim/sneak: update mappings

--- nvim/lua/plugins/sneak.lua
+++ nvim/lua/plugins/sneak.lua
@@ -1,6 +1,8 @@
 return {
     "justinmk/vim-sneak",
     keys = {
+        { "s", "<Plug>Sneak_s" },
+        { "S", "<Plug>Sneak_S" },
         { "f", "<Plug>Sneak_f" },
         { "F", "<Plug>Sneak_F" },
         { "t", "<Plug>Sneak_t" },
