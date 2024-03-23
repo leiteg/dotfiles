72e3e0d1a10ca4d504b8ca86ba83a6ef4037c736
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-23 14:04:04 -0300

    nvim/fugitive: update mappings

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -2,9 +2,9 @@ return {
     "tpope/vim-fugitive",
     cmd = "Git",
     keys = {
-        { "<leader><leader>", "<cmd>Git<CR>" },
+        { "<leader><leader>", "<cmd>vert Git<CR>" },
         { "<leader>gc",       "<cmd>tab Git commit<CR>" },
-        { "<leader>gl",       "<cmd>tab Git diff --staged<CR>" },
+        { "<leader>gr",       "<cmd>tab Git diff --staged<CR>" },
         { "<leader>gd",       "<cmd>tab Git diff<CR>" },
         { "<leader>gl",       "<cmd>tab Git log --oneline<CR>" },
         { "<leader>gb",       "<cmd>tab Git blame<CR>" },
