248347a013a04f33228edc409c7d6f435d7c5359
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-28 13:07:41 -0300

    nvim/noice: disable custom messages ui

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,7 +1,11 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        messages = {
+            enabled = false,
+        },
+    },
     dependencies = {
         "MunifTanjim/nui.nvim",
         -- "rcarriga/nvim-notify",
