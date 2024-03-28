5d4a970e756b11529753c4c8ded1ca46f15252e5
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-28 12:55:24 -0300

    nvim: add `echasnovski/mini.ai`

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        -- Autopairs
+        "echasnovski/mini.pairs",
+        version = false,
+        opts = {},
+    },
+    {
+        -- Extended a/i textobjects
+        "echasnovski/mini.ai",
+        version = false,
+        opts = {},
+    }
+}
