f824dbb4d372e873a36ff57bba0f72fb19fb88a3
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-28 12:57:45 -0300

    nvim: add `echasnovski/mini.align`

--- nvim/lua/plugins/easy-align.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "junegunn/vim-easy-align",
-    keys = {
-        { "ga", "<Plug>(EasyAlign)", mode = "n" },
-        { "ga", "<Plug>(EasyAlign)", mode = "x" },
-    }
-}
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -10,5 +10,11 @@ return {
         "echasnovski/mini.ai",
         version = false,
         opts = {},
-    }
+    },
+    {
+        -- Align text
+        "echasnovski/mini.align",
+        version = false,
+        opts = {},
+    },
 }
