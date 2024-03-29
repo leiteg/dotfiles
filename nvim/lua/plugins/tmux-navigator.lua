baf989e74104a161abed8b02d25339c0a6654198
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-29 18:31:23 -0300

    nvim/tmux: update tmux navigator mappings

--- nvim/lua/plugins/tmux-navigator.lua
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -8,10 +8,9 @@ return {
         "TmuxNavigatePrevious",
     },
     keys = {
-        { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-        { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-        { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        { "<A-h>",  "<cmd>TmuxNavigateLeft<cr>" },
+        { "<A-j>",  "<cmd>TmuxNavigateDown<cr>" },
+        { "<A-k>",  "<cmd>TmuxNavigateUp<cr>" },
+        { "<A-l>",  "<cmd>TmuxNavigateRight<cr>" },
     },
 }
