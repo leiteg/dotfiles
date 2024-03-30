6f7da3d23675c748e8407bc97e1421ac53c3d7e6
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-03-30 01:32:56 -0300

    nvim: import local plugins

---
+++ nvim/lua/plugins/local/init.lua
@@ -0,0 +1,8 @@
+--[[
+
+    Files in this folder are untracked but are imported by lazy. Use this
+    directory in order to load plugins that are specific to this machine.
+    This file must be kept empty.
+
+--]]
+return {}
