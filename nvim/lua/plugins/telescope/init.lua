48f1e7f04745ff1935dc619c32ba227956095d17
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-01 15:21:08 -0300

    nvim/telescope: update find all files picker

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -32,9 +32,17 @@ return {
             },
         })
 
+        local find_all_files = function()
+            builtin.find_files({
+                hidden = true,
+                no_ignore = true,
+            })
+        end
+
         -- See :h telescope.builtin
         vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope Git Files", })
-        vim.keymap.set("n", "g<C-P>", builtin.find_files, { desc = "Telescope All Files", })
+        vim.keymap.set("n", "g<C-P>", find_all_files, { desc = "Telescope All Files", })
+        vim.keymap.set("n", "<leader>p", find_all_files, { desc = "Telescope All Files", })
         vim.keymap.set("n", "<leader>ls", builtin.buffers, { desc = "Telescope Buffers", })
         vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "Telescope Live Grep", })
         vim.keymap.set("n", "<leader>td", builtin.diagnostics, { desc = "Telescope Diagnostics", })
