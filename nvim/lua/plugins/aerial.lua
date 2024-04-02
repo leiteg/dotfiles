c1578ef68787cc85e656b022c9404cd820d011c0
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-02 19:42:35 -0300

    nvim/aerial: update options

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -1,12 +1,16 @@
 return {
     'stevearc/aerial.nvim',
     lazy = false,
-    opts = {},
+    opts = {
+        autojump = true,
+        close_on_select = true,
+        highlight_on_jump = false,
+    },
     keys = {
         { "<leader>q", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial" }
     },
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
+        "nvim-tree/nvim-web-devicons",
     },
 }
