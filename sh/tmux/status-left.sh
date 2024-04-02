88d034732ef7508dab432b46b4341856ba3e6d82
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-02 00:59:47 -0300

    tmux: update theme

--- sh/tmux/status-left.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. $HOME/dotfiles/sh/tmux/common.sh
-
-BG=141 FG=59 add_section_left "#{?client_prefix,󰮄,󰆧} #S"
--- sh/tmux/status-right.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-. $HOME/dotfiles/sh/tmux/common.sh
-
-BG=colour215 FG=colour0 add_section_right $USER
-BG=colour212 FG=colour0 add_section_right $HOSTNAME
-BG=colour141 FG=colour0 add_section_right "󰥔  $(date +"%a %b %d %H:%M")"
