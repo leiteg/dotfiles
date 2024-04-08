696f6f75852ba07343b7fcc424c678752dc22ca3
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-07 22:59:02 -0300

    nvim: standardize function call motions

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -5,6 +5,10 @@ return {
     config = function()
         require("nvim-surround").setup {
             move_cursor = false,
+            aliases = {
+                -- Use `c` to surround text with function call.
+                ["c"] = "f",
+            },
         }
         vim.cmd [[ highlight link NvimSurroundHighlight IncSearch ]]
     end
