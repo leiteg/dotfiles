28fe00c7d966de963c4729d74ab45f3d240f296a
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-07 22:23:14 -0300

    nvim: add `utilyre/barbecue.nvim`

---
+++ nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,11 @@
+-- VSCode-like winbar for Neovim.
+return {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+        "SmiteshP/nvim-navic",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+}
