22a42141401725e8194b15bff5ed381fd1a7e522
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-09 15:09:12 -0300

    nvim: `tpope/vim-fugitive` -> `NeogitOrg/neogit`

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,16 @@
+return {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    keys = {
+        { "<leader><leader>", "<cmd>Neogit<cr>",            desc = "Neogit" },
+        { "<leader>gc",       "<cmd>tab Neogit commit<CR>", desc = "Neogit commit" },
+        { "<leader>gd",       "<cmd>tab Neogit diff<CR>",   desc = "Neogit diff" },
+        { "<leader>gl",       "<cmd>tab Neogit log<CR>",    desc = "Neogit log" },
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    opts = {},
+}
