e6cc2ef9118aa1005c8baef6fa04bd853f15bd25
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-11 16:13:27 -0300

    nvim/aerial: update config and keymaps

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -8,9 +8,20 @@ return {
         autojump = true,
         close_on_select = true,
         highlight_on_jump = false,
+        show_guides = true,
+        keymaps = {
+            ["<esc>"] = "actions.close",
+        },
+        nav = {
+            keymaps = {
+                ["q"] = "actions.close",
+                ["<esc>"] = "actions.close",
+            },
+        },
     },
     keys = {
-        { "<leader>q", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial" }
+        { "<leader>q", "<cmd>AerialToggle float<CR>", desc = "Aerial Toggle" },
+        { "<leader>Q", "<cmd>AerialNavToggle<CR>",    desc = "Aerial Toggle Nav" },
     },
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
