90ed4b4c513fc12e3ece285e0f937d99eb01ba3d
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-13 14:07:26 -0300

    nvim/neodev: add explicit options

--- nvim/lua/plugins/ft/lua/neodev.lua
+++ nvim/lua/plugins/ft/lua/neodev.lua
@@ -1,4 +1,14 @@
 return {
     "folke/neodev.nvim",
-    opts = {},
+    opts = {
+        library = {
+            enabled = true,
+            runtime = true,
+            types = true,
+            plugins = true,
+        },
+        setup_jsonls = true,
+        lspconfig = true,
+        pathStrict = true,
+    },
 }
