e8d9ac7f860e5b1a305cd1366c1e9a6ca4bcbef5
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-15 12:41:11 -0300

    nvim/gitsigns: add hunk navigation keybindings

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -12,12 +12,14 @@ return {
     "lewis6991/gitsigns.nvim",
     lazy = false,
     keys = {
-        { "<leader>gs", _ "stage_hunk",      desc = "Gitsigns Hunk Stage" },
-        { "<leader>gu", _ "undo_stage_hunk", desc = "Gitsigns Hunk Stage Undo" },
-        { "<leader>gr", _ "reset_hunk",      desc = "Gitsigns Hunk Reset" },
-        { "<leader>gp", _ "preview_hunk",    desc = "Gitsigns Hunk Preview" },
-        { "<leader>gt", _ "toggle_deleted",  desc = "Gitsigns Deleted" },
-        { "<leader>gb", _ "blame_line",      desc = "Gitsigns Blame" },
+        { "<leader>gs", _ "stage_hunk",        desc = "Gitsigns Hunk Stage" },
+        { "<leader>gu", _ "undo_stage_hunk",   desc = "Gitsigns Hunk Stage Undo" },
+        { "<leader>gr", _ "reset_hunk",        desc = "Gitsigns Hunk Reset" },
+        { "<leader>gp", _ "preview_hunk",      desc = "Gitsigns Hunk Preview" },
+        { "<leader>gt", _ "toggle_deleted",    desc = "Gitsigns Deleted" },
+        { "<leader>gb", _ "blame_line",        desc = "Gitsigns Blame" },
+        { "]h",         _("nav_hunk", "next"), desc = "Gitsigns Hunk Next" },
+        { "[h",         _("nav_hunk", "prev"), desc = "Gitsigns Hunk Prev" },
     },
     opts = {
         -- Display sign column for staged hunks, with a dimer color.
