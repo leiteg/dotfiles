7e8e713161f28c6afcea496b6c656b3fd4d8aa9e
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-04-30 21:07:37 -0300

    nvim/snippets: add snippet for mit license header

--- nvim/snippets/all.lua
+++ nvim/snippets/all.lua
@@ -13,6 +13,7 @@ local utils = require("core.lib.sniputils")
 local snippet = utils.snippet
 
 -- Luasnip
+local i = ls.i
 local t = ls.t
 local c = ls.c
 local f = ls.f
@@ -37,6 +38,31 @@ local snippets = {
         s = f(function()
             return os.date("%Y-%m-%d")
         end)
+    }),
+
+    snippet("mit", "MIT License Header", [[
+        Copyright (c) <year> <holders>
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of
+        this software and associated documentation files (the "Software"), to deal in
+        the Software without restriction, including without limitation the rights to
+        use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+        of the Software, and to permit persons to whom the Software is furnished to do
+        so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+    ]], {
+        year = i(1, "2024"),
+        holders = i(2, "Gustavo Leite")
     }),
 
 }
