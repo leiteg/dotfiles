98e9bd81e5257f583ae2aefb1dac9ef1d68e0c43
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-05-13 13:39:14 -0300

    refactor: commands

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -1,15 +1,16 @@
-local open_dotfiles = function()
+--------------------------------------------------------------------------------
+--                                                    __       __             --
+--    _________  ____ ___  ____ ___  ____ _____  ____/ /____  / /_  ______ _  --
+--   / ___/ __ \/ __ `__ \/ __ `__ \/ __ `/ __ \/ __  / ___/ / / / / / __ `/  --
+--  / /__/ /_/ / / / / / / / / / / / /_/ / / / / /_/ (__  ) / / /_/ / /_/ /   --
+--  \___/\____/_/ /_/ /_/_/ /_/ /_/\__,_/_/ /_/\__,_/____(_)_/\__,_/\__,_/    --
+--                                                                            --
+--------------------------------------------------------------------------------
+
+vim.api.nvim_create_user_command("Dotfiles", function()
     vim.cmd [[ tabedit ~/dotfiles ]]
-end
+end, { desc = "Open Dotfiles in a new tab.", })
 
-local open_plugins = function()
+vim.api.nvim_create_user_command("Plugins", function()
     vim.cmd [[ tabedit ~/dotfiles/nvim/lua/plugins ]]
-end
-
-vim.api.nvim_create_user_command("Dotfiles", open_dotfiles, {
-    desc = "Open Dotfiles in a new tab.",
-})
-
-vim.api.nvim_create_user_command("Plugins", open_plugins, {
-    desc = "Open plugins directory in a new tab.",
-})
+end, { desc = "Open plugins directory in a new tab.", })
