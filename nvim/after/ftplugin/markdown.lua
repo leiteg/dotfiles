c7ab793d8052b569550486d6fa32e6555daf39e9
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-08-23 19:04:21 -0300

    feat(nvim): add markdown ftplugin

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,10 @@
+vim.wo.wrap = true
+vim.wo.linebreak = true
 vim.wo.conceallevel = 2
+vim.bo.textwidth = 0
+vim.bo.wrapmargin = 0
+
+local set = vim.keymap.set
+
+set("n", "j", "gj", { buffer = true })
+set("n", "k", "gk", { buffer = true })
