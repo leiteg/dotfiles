8b9aea22fab7559f36b46dafea27d37a5b8726be
Gustavo Leite <gustavoleite.ti@gmail.com>
2024-10-16 17:18:10 -0300

    feat(nvim): update `quicker.nvim` options

--- nvim/lua/plugins/quicker.lua
+++ nvim/lua/plugins/quicker.lua
@@ -1,8 +1,24 @@
 --[[
 -- Improved UI and workflow for the Neovim quickfix.
 --]]
+
+local function _(fn, args)
+    return function()
+        require("quicker")[fn](args)
+    end
+end
+
 return {
     "stevearc/quicker.nvim",
     event = "FileType qf",
-    opts = {},
+    opts = {
+        constrain_cursor = false,
+        keys = {
+            { ">",    _ "expand",     desc = "Expand quickfix content" },
+            { "<",    _ "collapse",   desc = "Collapse quickfix content" },
+            { "q", "<cmd>cclose<CR>", desc = "Close quickfix" },
+            -- FIXME: This should work by default. Problem is being caused by other plugin though.
+            { "<CR>", "<cmd>.cc<CR>", desc = "Jump to error under the cursor" },
+        },
+    },
 }
