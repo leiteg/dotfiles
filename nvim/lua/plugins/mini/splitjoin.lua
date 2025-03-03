2f8ea95550fd7c52485c6d6257c826b105f08603
Gustavo Leite <gustavoleite.ti@gmail.com>
2025-03-03 15:23:43 -0300

    feat(nvim): add plugin `mini.splitjoin`

---
+++ nvim/lua/plugins/mini/splitjoin.lua
@@ -0,0 +1,15 @@
+--[[
+-- Split and join arguments
+--]]
+return {
+    "echasnovski/mini.splitjoin",
+    event = "VeryLazy",
+    version = false,
+    opts = {
+        mappings = {
+            toggle = "<leader>m",
+            split = "",
+            join = "",
+        },
+    },
+}
