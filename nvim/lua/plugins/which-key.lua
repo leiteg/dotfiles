00caeec5fe8f482c95e11c906646a99b842872a9
Gustavo Leite <gustavoleite.ti@gmail.com>
2025-03-24 19:19:20 -0300

    feat(nvim): update `which-keys` config

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,12 +2,25 @@
 -- Create key bindings that stick. WhichKey is a lua plugin for Neovim 0.5 that
 -- displays a popup with possible keybindings of the command you started typing.
 --]]
+
+local function show_local()
+    require("which-key").show({})
+end
+
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     init = function()
         vim.o.timeout = true
         vim.o.timeoutlen = 300
+        require("which-key").add({
+            { "<leader>d", group = "Debug" },
+            { "<leader>g", group = "Git" },
+            { "<leader>o", group = "Obsidian" },
+        })
     end,
+    keys = {
+        { "<leader>?", show_local, desc = "Whichkey Show Local Mappings" },
+    },
     opts = {}
 }
