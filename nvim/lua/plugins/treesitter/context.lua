19b90c96e261b557ef15c8f1ecbfb9f60c48d797
Gustavo Leite <gustavoleite.ti@gmail.com>
2025-06-04 13:54:09 -0300

    feat(nvim): update `nvim-treesitter-context` options

--- nvim/lua/plugins/treesitter/context.lua
+++ nvim/lua/plugins/treesitter/context.lua
@@ -1,6 +1,17 @@
 --[[
 -- Show code context.
 --]]
+
+local function goto_context()
+    require("treesitter-context").go_to_context(vim.v.count1)
+end
+
 return {
-    "nvim-treesitter/nvim-treesitter-context"
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+        max_lines = 3,
+    },
+    keys = {
+        { "[c", goto_context, desc = "Go to context" },
+    },
 }
